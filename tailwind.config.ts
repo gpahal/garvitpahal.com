c3d99faef1d253b94445875d84ea89f982870a6d
Garvit Pahal <g10pahal@gmail.com>
2025-03-31 01:16:02 +0530

    Update dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,7 @@
 import type { Config } from 'tailwindcss'
 import { fontFamily } from 'tailwindcss/defaultTheme'
 
-import tailwindcssColorThemes from '@gpahal/tailwindcss-color-themes'
 import tailwindcssVariants from '@gpahal/tailwindcss-variants'
-
-import { darkTheme, lightTheme } from './tailwind/color-theme'
 
 const config: Config = {
   content: ['./src/**/*.{astro,js,ts,jsx,tsx,mdx}'],
@@ -21,20 +18,20 @@ const config: Config = {
   },
   plugins: [
     tailwindcssVariants,
-    tailwindcssColorThemes({
-      default: lightTheme,
-      defaultDark: darkTheme,
-      selections: [
-        {
-          selector: '.light-theme',
-          theme: lightTheme,
-        },
-        {
-          selector: '.dark-theme',
-          theme: darkTheme,
-        },
-      ],
-    }),
+    // tailwindcssColorThemes({
+    //   default: lightTheme,
+    //   defaultDark: darkTheme,
+    //   selections: [
+    //     {
+    //       selector: '.light-theme',
+    //       theme: lightTheme,
+    //     },
+    //     {
+    //       selector: '.dark-theme',
+    //       theme: darkTheme,
+    //     },
+    //   ],
+    // }),
   ],
 }
 
